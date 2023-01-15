241ae2b405c88a690a53688252791c0e41d76a96
namikaze-dev <usmansadik96@gmail.com>
2023-01-15 22:09:46 +0100

    feat: update health to return json

--- cmd/api/health.go
+++ cmd/api/health.go
@@ -6,7 +6,8 @@ import (
 )
 
 func (app *application) healthcheckHandler(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprintln(w, "status: available")
-	fmt.Fprintf(w, "environment: %s\n", app.config.env)
-	fmt.Fprintf(w, "version: %s\n", version)
+	resp := `{"status": "available", "environment": %q, "version": %q}`
+	resp = fmt.Sprintf(resp, app.config.env, version)
+	w.Header().Set("Content-Type", "application/json")
+	w.Write([]byte(resp))
 }
