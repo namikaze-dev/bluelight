d341671fc2ac85e00c5ab402ac531bff14334051
namikaze-dev <usmansadik96@gmail.com>
2023-01-15 22:41:51 +0100

    refactor: update health endpoint

--- cmd/api/health.go
+++ cmd/api/health.go
@@ -1,13 +1,24 @@
 package main
 
 import (
-	"fmt"
+	"encoding/json"
 	"net/http"
 )
 
 func (app *application) healthHandler(w http.ResponseWriter, r *http.Request) {
-	resp := `{"status": "available", "environment": %q, "version": %q}`
-	resp = fmt.Sprintf(resp, app.config.env, version)
+	data := map[string]string{
+		"status":      "available",
+		"environment": app.config.env,
+		"version":     version,
+	}
+
+	resp, err := json.Marshal(data)
+	if err != nil {
+		app.logger.Println(err)
+		http.Error(w, "The server encountered a problem and could not process your request", http.StatusInternalServerError)
+		return
+	}
+
 	w.Header().Set("Content-Type", "application/json")
-	w.Write([]byte(resp))
+	w.Write(resp)
 }
