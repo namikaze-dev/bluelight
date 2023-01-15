2f19aba3390c3963044c653137be953954dea327
namikaze-dev <usmansadik96@gmail.com>
2023-01-15 22:55:11 +0100

    feat: add helper to write json response, add to health handler

--- cmd/api/health.go
+++ cmd/api/health.go
@@ -1,7 +1,6 @@
 package main
 
 import (
-	"encoding/json"
 	"net/http"
 )
 
@@ -12,13 +11,9 @@ func (app *application) healthHandler(w http.ResponseWriter, r *http.Request) {
 		"version":     version,
 	}
 
-	resp, err := json.Marshal(data)
+	err := app.writeJSON(w, http.StatusOK, data, nil)
 	if err != nil {
 		app.logger.Println(err)
 		http.Error(w, "The server encountered a problem and could not process your request", http.StatusInternalServerError)
-		return
 	}
-
-	w.Header().Set("Content-Type", "application/json")
-	w.Write(resp)
 }
