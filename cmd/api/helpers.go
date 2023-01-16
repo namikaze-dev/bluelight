7db84687c7a91ea4224ae096252d4c7fb6208fec
namikaze-dev <usmansadik96@gmail.com>
2023-01-16 23:01:28 +0100

    feat: add helper method to retrieve id from http req

--- cmd/api/helpers.go
+++ cmd/api/helpers.go
@@ -2,8 +2,25 @@ package main
 
 import (
 	"encoding/json"
+	"errors"
 	"net/http"
+	"strconv"
+
+	"github.com/julienschmidt/httprouter"
 )
+
+var (
+	ErrInvalidIdParam = errors.New("invalid id parameter")
+)
+
+func (app *application) readIDParam(r *http.Request) (int64, error) {
+	params := httprouter.ParamsFromContext(r.Context())
+	id, err := strconv.ParseInt(params.ByName("id"), 10, 64)
+	if err != nil || id < 1 {
+		return 0, ErrInvalidIdParam
+	}
+	return id, nil
+}
 
 func (app *application) writeJSON(w http.ResponseWriter, status int, data interface{}, headers http.Header) error {
 	js, err := json.Marshal(data)
