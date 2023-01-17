11bf8bc07cfe7d7dd80ab02aa3934bd2e0b67804
namikaze-dev <usmansadik96@gmail.com>
2023-01-17 22:57:49 +0100

    feat: add validate method for movie model

--- internal/models/movies.go
+++ internal/models/movies.go
@@ -6,6 +6,8 @@ import (
 	"strconv"
 	"strings"
 	"time"
+
+	"github.com/namikaze-dev/bluelight/internal/validator"
 )
 
 var ErrInvalidRuntimeFormat = errors.New("invalid runtime format")
@@ -46,4 +48,18 @@ func (r *Runtime) UnmarshalJSON(jsonValue []byte) error {
 
 	*r = Runtime(i)
 	return nil
+}
+
+func ValidateMovie(v *validator.Validator, movie *Movie) {
+	v.Check(movie.Title != "", "title", "must be provided")
+	v.Check(len(movie.Title) <= 500, "title", "must not be more than 500 bytes long")
+	v.Check(movie.Year != 0, "year", "must be provided")
+	v.Check(movie.Year >= 1888, "year", "must be greater than 1888")
+	v.Check(movie.Year <= int32(time.Now().Year()), "year", "must not be in the future")
+	v.Check(movie.Runtime != 0, "runtime", "must be provided")
+	v.Check(movie.Runtime > 0, "runtime", "must be a positive integer")
+	v.Check(movie.Genres != nil, "genres", "must be provided")
+	v.Check(len(movie.Genres) >= 1, "genres", "must contain at least 1 genre")
+	v.Check(len(movie.Genres) <= 5, "genres", "must not contain more than 5 genres")
+	v.Check(validator.Unique(movie.Genres), "genres", "must not contain duplicate values")
 }
