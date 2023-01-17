559afb8107e5fdefd7b04a67d53ce33ec59073a5
namikaze-dev <usmansadik96@gmail.com>
2023-01-17 21:50:43 +0100

    feat: add error helpers

--- internal/models/movies.go
+++ internal/models/movies.go
@@ -1,15 +1,26 @@
 package models
 
 import (
+	"fmt"
+	"strconv"
 	"time"
 )
+
+type Runtime int32
 
 type Movie struct {
 	ID        int64     `json:"id"`
 	CreatedAt time.Time `json:"-"`
 	Title     string    `json:"title"`
 	Year      int32     `json:"year,omitempty"`
-	Runtime   int32     `json:"runtime,omitempty"` // (in minutes)
+	Runtime   Runtime   `json:"runtime,omitempty,string"`
 	Genres    []string  `json:"genres,omitempty"`
 	Version   int32     `json:"version"`
+}
+
+
+func (r Runtime) MarshalJSON() ([]byte, error) {
+	jsonValue := fmt.Sprintf("%d mins", r)
+	quotedJSONValue := strconv.Quote(jsonValue)
+	return []byte(quotedJSONValue), nil
 }
