6ef893ab5819c4f10ed566273cc4841387e41508
namikaze-dev <usmansadik96@gmail.com>
2023-01-19 10:42:59 +0100

    test: add test for create movie endpoint

--- cmd/api/movies_test.go
+++ cmd/api/movies_test.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"bytes"
 	"encoding/json"
 	"io"
 	"net/http"
@@ -42,4 +43,100 @@ func TestShowMovie(t *testing.T) {
 
 	var want int64 = 1
 	assertEqual(t, got.Movie.ID, want)
+}
+
+func TestCreateMovie(t *testing.T) {
+	app := &application{
+		config: config{
+			env: "development",
+		},
+	}
+
+	ts := httptest.NewServer(app.routes())
+	defer ts.Close()
+
+	var input struct {
+		Title   string   `json:"title"`
+		Year    int      `json:"year"`
+		Runtime string   `json:"runtime"`
+		Genres  []string `json:"genres"`
+	}
+
+	// test for bad request
+	input.Title = "undead"
+	input.Genres = []string{"action"}
+	input.Year = 2020
+	// missing runtime field should yield a bad request response
+
+	b, err := json.Marshal(input)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	rd := bytes.NewReader(b)
+	resp, err := ts.Client().Post(ts.URL+"/v1/movies", "application/json", rd)
+	if err != nil {
+		t.Fatal(err)
+	}
+	defer resp.Body.Close()
+	assertEqual(t, resp.StatusCode, http.StatusBadRequest)
+
+	// test for unprocessible entity
+	input.Title = "undead"
+	input.Genres = []string{"action", "action"}
+	input.Year = 2020
+	input.Runtime = "100 mins"
+	// duplicates genres field should yield a unprocessible entity response
+	b, err = json.Marshal(input)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	rd = bytes.NewReader(b)
+	resp, err = ts.Client().Post(ts.URL+"/v1/movies", "application/json", rd)
+	if err != nil {
+		t.Fatal(err)
+	}
+	defer resp.Body.Close()
+	assertEqual(t, resp.StatusCode, http.StatusUnprocessableEntity)
+
+	// test for ok
+	input.Title = "undead"
+	input.Genres = []string{"action", "horror"}
+	input.Year = 2020
+	input.Runtime = "100 mins"
+	b, err = json.Marshal(input)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	rd = bytes.NewReader(b)
+	resp, err = ts.Client().Post(ts.URL+"/v1/movies", "application/json", rd)
+	if err != nil {
+		t.Fatal(err)
+	}
+	defer resp.Body.Close()
+	assertEqual(t, resp.StatusCode, http.StatusOK)
+
+	// check response body
+	body, err := io.ReadAll(resp.Body)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	var got struct {
+		Movie struct {
+			Runtime string `json:"runtime"`
+			Title   string `json:"title"`
+			Year    int    `json:"year"`
+		} `json:"movie"`
+	}
+	err = json.Unmarshal(body, &got)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	assertEqual(t, got.Movie.Runtime, input.Runtime)
+	assertEqual(t, got.Movie.Year, input.Year)
+	assertEqual(t, got.Movie.Title, input.Title)
 }
