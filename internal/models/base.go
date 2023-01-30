fca48331d56eca4fa33056617d620bc7666de663
namikaze-dev <usmansadik96@gmail.com>
2023-01-30 12:40:36 +0100

    feat: add movie interface to base model

--- internal/models/base.go
+++ internal/models/base.go
@@ -9,13 +9,23 @@ var (
 	ErrRecordNotFound = errors.New("record not found")
 )
 
-
 type Models struct {
-	Movies MovieModel
+	Movies interface {
+		Insert(movie *Movie) error
+		Get(id int64) (*Movie, error)
+		Update(movie *Movie) error
+		Delete(id int64) error
+	}
 }
 
-func NewModels(db *sql.DB) Models {
-	return Models{
-		Movies: MovieModel{DB: db},
+func NewModels(db *sql.DB) *Models {
+	return &Models{
+		Movies: &MovieModel{DB: db},
+	}
+}
+
+func NewMockModels() *Models {
+	return &Models{
+		Movies: &MockMovieModel{},
 	}
 }
