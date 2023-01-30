f65099f9d8f95fc947dc458467a0b689c51895f4
namikaze-dev <usmansadik96@gmail.com>
2023-01-30 21:39:58 +0100

    feat: update updateMovie handler to support partial updates

--- cmd/api/movies.go
+++ cmd/api/movies.go
@@ -94,10 +94,10 @@ func (app *application) updateMovieHandler(w http.ResponseWriter, r *http.Reques
 	}
 
 	var input struct {
-		Title   string         `json:"title"`
-		Year    int32          `json:"year"`
-		Runtime models.Runtime `json:"runtime"`
-		Genres  []string       `json:"genres"`
+		Title   *string         `json:"title"`
+		Year    *int32          `json:"year"`
+		Runtime *models.Runtime `json:"runtime"`
+		Genres  []string        `json:"genres"`
 	}
 
 	err = app.readJSON(w, r, &input)
@@ -106,10 +106,21 @@ func (app *application) updateMovieHandler(w http.ResponseWriter, r *http.Reques
 		return
 	}
 
-	movie.Title = input.Title
-	movie.Year = input.Year
-	movie.Runtime = input.Runtime
-	movie.Genres = input.Genres
+	if input.Title != nil {
+		movie.Title = *input.Title
+	}
+
+	if input.Year != nil {
+		movie.Year = *input.Year
+	}
+
+	if input.Runtime != nil {
+		movie.Runtime = *input.Runtime
+	}
+
+	if input.Genres != nil {
+		movie.Genres = input.Genres 
+	}
 
 	v := validator.New()
 	if models.ValidateMovie(v, movie); !v.Valid() {
@@ -135,7 +146,7 @@ func (app *application) deleteMovieHandler(w http.ResponseWriter, r *http.Reques
 		app.notFoundResponse(w, r)
 		return
 	}
-	
+
 	err = app.models.Movies.Delete(id)
 	if err != nil {
 		switch {
