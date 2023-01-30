215eac6a6820fb5cd00b000ad25fdb8107949313
namikaze-dev <usmansadik96@gmail.com>
2023-01-30 19:44:24 +0100

    test: add test for update and delete movie endpoints

--- cmd/api/movies_test.go
+++ cmd/api/movies_test.go
@@ -156,3 +156,104 @@ func TestCreateMovie(t *testing.T) {
 	assertEqual(t, got.Movie.Year, input.Year)
 	assertEqual(t, got.Movie.Title, input.Title)
 }
+
+func TestUpdateMovie(t *testing.T) {
+	app := &application{
+		config: config{
+			env: "development",
+		},
+		models: models.NewMockModels(),
+	}
+
+	ts := httptest.NewServer(app.routes())
+	defer ts.Close()
+
+	var input struct {
+		Title   string   `json:"title"`
+		Year    int      `json:"year"`
+		Runtime string   `json:"runtime"`
+		Genres  []string `json:"genres"`
+	}
+
+	input.Title = "undead"
+	input.Genres = []string{"action", "horror"}
+	input.Year = 2020
+	input.Runtime = "100 mins"
+	b, err := json.Marshal(input)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	rd := bytes.NewReader(b)
+	// movie with id 1 available by default
+	req, err := http.NewRequest(http.MethodPut, ts.URL+"/v1/movies/1", rd)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	resp, err := ts.Client().Do(req)
+	if err != nil {
+		t.Fatal(err)
+	}
+	defer resp.Body.Close()
+	assertEqual(t, resp.StatusCode, http.StatusOK)
+
+	body, err := io.ReadAll(resp.Body)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	var got struct {
+		Movie struct {
+			Runtime string `json:"runtime"`
+			Title   string `json:"title"`
+			Year    int    `json:"year"`
+		} `json:"movie"`
+	}
+	err = json.Unmarshal(body, &got)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	assertEqual(t, got.Movie.Runtime, input.Runtime)
+	assertEqual(t, got.Movie.Year, input.Year)
+	assertEqual(t, got.Movie.Title, input.Title)
+}
+
+func TestDeleteMovie(t *testing.T) {
+	app := &application{
+		config: config{
+			env: "development",
+		},
+		models: models.NewMockModels(),
+	}
+
+	ts := httptest.NewServer(app.routes())
+	defer ts.Close()
+
+	req, err := http.NewRequest(http.MethodDelete, ts.URL+"/v1/movies/10", nil)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	resp, err := ts.Client().Do(req)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	// movie of id 10 should not exist
+	assertEqual(t, resp.StatusCode, http.StatusNotFound)
+
+	// movie with id 1 available by default
+	req, err = http.NewRequest(http.MethodDelete, ts.URL+"/v1/movies/1", nil)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	resp, err = ts.Client().Do(req)
+	if err != nil {
+		t.Fatal(err)
+	}
+	defer resp.Body.Close()
+	assertEqual(t, resp.StatusCode, http.StatusOK)
+}
