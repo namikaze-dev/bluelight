0d105c45f724afc2f7fc0cc9bfe34fcf52a3a99e
namikaze-dev <usmansadik96@gmail.com>
2023-02-03 15:36:06 +0100

    feat: update tokens model to include auth scope

--- internal/models/tokens.go
+++ internal/models/tokens.go
@@ -12,15 +12,16 @@ import (
 )
 
 const (
-	ScopeActivation = "activation"
+	ScopeActivation     = "activation"
+	ScopeAuthentication = "authentication"
 )
 
 type Token struct {
-	Plaintext string
-	Hash      []byte
-	UserID    int64
-	Expiry    time.Time
-	Scope     string
+	Plaintext string    `json:"token"`
+	Hash      []byte    `json:"-"`
+	UserID    int64     `json:"-"`
+	Expiry    time.Time `json:"expiry"`
+	Scope     string    `json:"-"`
 }
 
 func generateToken(userID int64, ttl time.Duration, scope string) (*Token, error) {
@@ -83,7 +84,7 @@ func (m TokenModel) DeleteAllForUser(scope string, userID int64) error {
 
 	ctx, cancel := context.WithTimeout(context.Background(), 3*time.Second)
 	defer cancel()
-	
+
 	_, err := m.DB.ExecContext(ctx, query, scope, userID)
 	return err
 }
