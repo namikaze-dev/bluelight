f24f5f73e6a1aae343be40549d0303e05ee02d21
namikaze-dev <usmansadik96@gmail.com>
2023-02-03 16:13:15 +0100

    fix: update http method for create auth token handler

--- cmd/api/routes.go
+++ cmd/api/routes.go
@@ -23,7 +23,7 @@ func (app *application) routes() http.Handler {
 	router.HandlerFunc(http.MethodPost, "/v1/users", app.createUserHandler)
 	router.HandlerFunc(http.MethodPut, "/v1/users/activated", app.activateUserHandler)
 
-	router.HandlerFunc(http.MethodPut, "/v1/tokens/authentication", app.createAuthenticationTokenHandler)
+	router.HandlerFunc(http.MethodPost, "/v1/tokens/authentication", app.createAuthenticationTokenHandler)
 
 	return app.recoverPanic(app.rateLimit(app.authenticate(router)))
 }
